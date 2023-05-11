d692d852f70e32218c4dec53a2656bef08719f43
DmytroStepaniukGH <115084520+DmytroStepaniukGH@users.noreply.github.com>
2023-05-11 11:33:18 +0300

    Add files via upload

---
+++ backend/backend/locale_settings.py
@@ -0,0 +1,7 @@
+secret_key = 'django-insecure-eaiej)8#r^apizu%m-*ebg8_ktrlu)+!!m3a!8)@-zbzo(d-*f'
+
+email_host_user = 'millarissa.valdes@gmail.com'
+email_host_password = 'hnhvsdyahetuwbdw'
+
+
+frontend_host = 'http://127.0.0.1:8000'
